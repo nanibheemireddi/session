6f811a20517f1d4478df0e83f9f5a4fb24247c68
nani <srinani659@gmail.com>
2016-09-14 18:15:34 +0530

    adding some files

---
+++ assignment10.js
@@ -0,0 +1,29 @@
+var express = require('express');
+var app = express();
+var PORT = 8080;
+
+var middleware = {
+	requireAuthentication: function(req, res, next){
+		console.log('route hit');
+		next();
+	},
+	logger:function(req,res,next){
+		console.log('get');
+		next();
+	}
+}
+
+
+app.use(middleware.requireAuthentication);
+app.use(middleware.logger);
+
+app.get('/', function(req, res)
+{
+	res.send('this is the express program');
+});
+
+app.use(express.static(__dirname +"/public" ));
+
+app.listen(PORT, function(){
+	console.log('srever started on port  '+PORT);
+});
