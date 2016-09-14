b43b9970bf327715f128b72af8951fc4de172bff
nani <srinani659@gmail.com>
2016-09-14 12:09:56 +0530

    intial commit

---
+++ express.js
@@ -0,0 +1,31 @@
+var express = require('express');
+var app = express();
+var PORT = process.env.PORT || 8080;
+
+var middleware = require("./middleware.js");
+
+
+app.use(middleware.logger);
+
+app.get('/', function(req, res)
+{
+	res.send('this is the express program');
+});
+
+
+app.get('/about', function(req, res)
+{
+	res.send('about us!');
+});
+
+app.get('/nani',middleware.requireAuthentication,function(req, res)
+{
+	res.send('about nani');
+});
+
+app.use(express.static(__dirname + "/public"));
+//console.log(__dirname);
+app.listen(PORT, function()
+{
+	console.log('express srever started on port ' +PORT);
+});
